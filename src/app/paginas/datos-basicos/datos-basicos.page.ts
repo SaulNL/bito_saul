4843f94f78ff47be8032a9c42a94f9464cd92423
Kenedy Vazquez <vazquezkenedy@outlook.com>
2020-10-12 16:20:16 -0500

    correciones de imagen guardar y fecha

--- src/app/paginas/datos-basicos/datos-basicos.page.ts
+++ src/app/paginas/datos-basicos/datos-basicos.page.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import {NgForm} from '@angular/forms';
+import { NgForm } from '@angular/forms';
 import { MsPersonaModel } from './../../Modelos/MsPersonaModel';
 import { PersonaService } from '../../api/persona.service';
 import { LoadingController } from '@ionic/angular';
-import {ArchivoComunModel} from '../../Modelos/ArchivoComunModel';
+import { ArchivoComunModel } from '../../Modelos/ArchivoComunModel';
 import { UtilsCls } from './../../utils/UtilsCls';
+import * as moment from 'moment';
+
 
 
 
@@ -16,12 +18,22 @@ import { UtilsCls } from './../../utils/UtilsCls';
 export class DatosBasicosPage implements OnInit {
   public usuarioSistema: MsPersonaModel;
   public loader: any;
+  public minDate: any;
+  public maxDate: any;
+  private file_img_galeria: FileList;
+
 
   constructor(
     private servicioPersona: PersonaService,
     public loadingController: LoadingController,
     private utilsCls: UtilsCls
-  ) { }
+  ) {
+    const currentYear = new Date().getFullYear();
+    this.minDate = new Date(currentYear - 100, 0, 0);
+    this.maxDate = new Date(currentYear - 18, 0, 0);
+    this.minDate = moment.parseZone(this.minDate).format("YYYY-MM-DD");
+    this.maxDate = moment.parseZone(this.maxDate).format("YYYY-MM-DD");
+  }
 
   ngOnInit() {
     this.usuarioSistema = new MsPersonaModel();
@@ -32,20 +44,18 @@ export class DatosBasicosPage implements OnInit {
       cssClass: 'my-custom-class',
       message: 'por favor espera...'
     });
-   return this.loader.present();
+    return this.loader.present();
   }
   private actualizarUsuario() {
     this.usuarioSistema = JSON.parse(localStorage.getItem('u_data'));
     this.usuarioSistema.fecha_nacimiento = this.usuarioSistema.fecha_nacimiento !== null ? new Date(this.usuarioSistema.fecha_nacimiento) : null;
-    console.log(this.usuarioSistema.fecha_nacimiento.toLocaleDateString());
-    this.usuarioSistema.fecha_nacimiento = this.usuarioSistema.fecha_nacimiento.toLocaleDateString();
   }
   actualizarDatos(formBasicos: NgForm) {
     this.presentLoading();
     const miPrimeraPromise = new Promise((resolve, reject) => {
       this.servicioPersona.guardar(this.usuarioSistema).subscribe(
         data => {
-          if(data.code === 200){
+          if (data.code === 200) {
             this.loader.dismiss();
             //this.notificacionService.pushAlert(data.message); 
           }
@@ -53,7 +63,7 @@ export class DatosBasicosPage implements OnInit {
           //this.router.navigate(['/home/mi/cuenta/inicio']);
           this.loader.dismiss();
           //this.notificacionService.pushInfo(data.data.mensaje);
-         // resolve(resultado);
+          // resolve(resultado);
 
         },
         error => {
@@ -66,44 +76,27 @@ export class DatosBasicosPage implements OnInit {
     miPrimeraPromise.then((successMessage) => {
     });
   }
-  public subir_imagen_cuadrada(event) {
-    if (event.target.files && event.target.files.length) {
-      let height;
-      let width;
-      for (const archivo of event.target.files) {
-        const reader = new FileReader();
-        reader.readAsDataURL(archivo);
-        reader.onload = () => {
-          const img = new Image();
-          img.src = reader.result as string;
-          img.onload = () => {
-          height = img.naturalHeight;
-          width = img.naturalWidth;
-          if (width === 400 && height === 400) {
-            const file_name = archivo.name;
-            const file = archivo;
-            if (file.size < 3145728) {
-              let file_64: any;
-              const utl = new UtilsCls();
-              utl.getBase64(file).then(
-                data => {
-                  const archivo = new ArchivoComunModel();
-                  if (file_name != null) {
-                    archivo.nombre_archivo = this.utilsCls.convertir_nombre(file_name);
-                    archivo.archivo_64 = file_64;
-                  }
-                  this.usuarioSistema.selfie = archivo;
-                }
-              );
-            } else {
-            //  this.notificacionService.pushAlert('comun.file_sobrepeso');
-            }
-          }else {
-            console.log('es mayor');
-          }
-        }
-      }
-    }
+  convercionFechaNac(event) {
+    let fecha = event.detail.value;
+    let ms = Date.parse(fecha);
+    fecha = new Date(ms);
+    this.usuarioSistema.fecha_nacimiento = fecha;
   }
- }
+
+  public subir_imagen_cuadrada(event) {
+    this.file_img_galeria = event.target.files;
+    const file_name = this.file_img_galeria[0].name;
+    const file = this.file_img_galeria[0];
+    let file_64: any;
+    const utl = new UtilsCls();
+    utl.getBase64(file).then(
+      data => {
+        file_64 = data;
+        const archivo = new ArchivoComunModel();
+        archivo.nombre_archivo = this.utilsCls.convertir_nombre(file_name);
+        archivo.archivo_64 = file_64;
+        this.usuarioSistema.selfie = archivo;
+      }
+    );
+  }
 }
