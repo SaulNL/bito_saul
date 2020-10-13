d63f524f0bbfc4fc1c59181103704e108b67074d
Kenedy Vazquez <vazquezkenedy@outlook.com>
2020-10-13 13:51:51 -0500

    banner-promociones

---
+++ src/app/componentes/banner-promciones/banner-promciones.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-banner-promciones',
+  templateUrl: './banner-promciones.component.html',
+  styleUrls: ['./banner-promciones.component.scss'],
+})
+export class BannerPromcionesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
