a99d454a88ef375cf4bc89e17887095bf9beba98
serch <serch0314@gmail.com>
2020-10-15 12:44:14 -0500

    Modulo de promociones

--- src/app/paginas/promociones/promociones.page.ts
+++ src/app/paginas/promociones/promociones.page.ts
@@ -1,12 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PromocionesService } from '../../api/promociones.service';
+import {LoadingController} from "@ionic/angular";
+/* Modelos */
+import { PromocionesModel } from '../../Modelos/PromocionesModel';
+import { FiltrosModel } from '../../Modelos/FiltrosModel';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'promociones.page.html',
   styleUrls: ['promociones.page.scss']
 })
-export class PromocionesPage {
+export class PromocionesPage implements OnInit {
 
-  constructor() {}
+  private loaderPrincipal: HTMLIonLoadingElement;
+  public lstPromociones: Array<PromocionesModel>;
+  public anyFiltros    : FiltrosModel;
+  public loader        : boolean = false;
 
+
+  constructor(private _promociones: PromocionesService,  public loadingController: LoadingController ) {
+    
+  }
+
+  ngOnInit(): void {
+    this.loader = true;
+    this.anyFiltros = new FiltrosModel();
+    this.lstPromociones = new Array<PromocionesModel>();
+    this.anyFiltros.kilometros = 1;
+    this.anyFiltros.idEstado = 29;
+    this.obtenerPromociones();
+  }
+
+  public obtenerPromociones() {
+    if (navigator.geolocation && this.anyFiltros.tipoBusqueda === 1) {
+      navigator.geolocation.getCurrentPosition(posicion => {
+        this.anyFiltros.latitud = posicion.coords.latitude;
+        this.anyFiltros.longitud = posicion.coords.longitude;
+        this.obtenerPromocionesServicio();
+      });
+    } else {
+      this.anyFiltros.tipoBusqueda = 0;
+      this.obtenerPromocionesServicio();
+    }
+  }
+
+  async presentLoading() {
+    const loading = await this.loadingController.create({
+        message: 'Cargando. . .',
+        duration: 5000
+    });
+    return await loading.present();
+  }
+
+  public obtenerPromocionesServicio() {
+    this.presentLoading().then(a => {});
+    this._promociones.buscarPromocinesPublicadasModulo(this.anyFiltros).subscribe(
+      response => {
+        if(response.code === 402){
+          console.log('402');
+          this.loaderPrincipal.onDidDismiss();
+        }
+        if (response.data !== null) {
+          this.lstPromociones = response.data;
+          this.loader = false;
+          this.loaderPrincipal.onDidDismiss();
+         // if(this.anyFiltros.strBuscar !== ""){this.modalMapBuscador()}
+        } else {
+          this.lstPromociones = [];
+          this.loaderPrincipal.onDidDismiss();
+        }
+      },
+      error => {
+        console.error(error);
+        this.lstPromociones = [];
+        this.loaderPrincipal.onDidDismiss();
+      },
+      () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      }
+    );
+  }
 }
