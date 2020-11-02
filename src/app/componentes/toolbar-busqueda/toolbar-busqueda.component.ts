d0e5fc1d7f2e8881a4f741a317d5ff08bf5c9f8d
Caleb Hernandez <tescorporations@gmail.com>
2020-11-02 14:06:44 -0600

    Se agrego la funcion de los permisos en boton ventas y compras

--- src/app/componentes/toolbar-busqueda/toolbar-busqueda.component.ts
+++ src/app/componentes/toolbar-busqueda/toolbar-busqueda.component.ts
@@ -1,4 +1,7 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Auth0Service} from '../../api/busqueda/auth0.service';
+import {NavBarServiceService} from '../../api/busqueda/nav-bar-service.service';
+import {SideBarService} from '../../api/busqueda/side-bar-service';
 
 @Component({
   selector: 'app-toolbar-busqueda',
@@ -9,10 +12,37 @@ export class ToolbarBusquedaComponent implements OnInit {
   @Input() public placeHolder: string = 'Buscar'
   @Output() public buscarEmit = new EventEmitter()
   strBuscar: String;
+  public user: any;
+  public permisos: Array<string>;
 
-  constructor() { }
+  constructor(
+    private _auth0: Auth0Service,
+    private navBarServiceService: NavBarServiceService,
+    private sideBarService: SideBarService
+  ) { 
+    this.permisos = [];
+    this.permisosList();
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    if (this.permisos.includes('ver_negocio')) {
+      //this.notificacionesVentas();
+      setInterval(it => {
+        //this.notificacionesVentas();
+      }, 300000);
+    }
+    this.user = this._auth0.getUserData();
+    this.navBarServiceService.change.subscribe(respuesta => {
+      this.user = respuesta;
+      this.permisos = ['ver_negocio'];
+    });
+
+    /* this.sideBarService.change.subscribe(isOpen => {
+      this.permisosList();
+    }); */
+    console.log(this.user.nombre);
+    
+  }
 
   buscar() {
     console.log(this.strBuscar)
@@ -22,5 +52,16 @@ export class ToolbarBusquedaComponent implements OnInit {
   limpiar() {
     this.strBuscar = null;
     this.buscar();
+  }
+
+  private permisosList() {
+    const permisos = JSON.parse(localStorage.getItem('u_permisos'));
+    const list = [];
+    if (permisos != null) {
+      permisos.map(p => {
+        list.push(p.nombre);
+      });
+      this.permisos = list;
+    }
   }
 }
