e1b1d393888c1f201a25d4f913cea75d4f105d86
Juan A <esjhuan1000@gmail.com>
2021-07-06 17:24:50 -0500

    muchos cambios menores

--- src/app/componentes/dar-like-negocio/dar-like-negocio.component.ts
+++ src/app/componentes/dar-like-negocio/dar-like-negocio.component.ts
@@ -1,5 +1,7 @@
-import { UtilsCls } from "./../../utils/UtilsCls";
+import { ProveedorServicioService } from "./../../api/busqueda/proveedores/proveedor-servicio.service";
+import { ToadNotificacionService } from "./../../api/toad-notificacion.service";
 import { Component, Input, OnInit } from "@angular/core";
+import { MsNegocioModel } from "../../Modelos/busqueda/MsNegocioModel";
 
 @Component({
   selector: "app-dar-like-negocio",
@@ -8,12 +10,30 @@ import { Component, Input, OnInit } from "@angular/core";
 })
 export class DarLikeNegocioComponent implements OnInit {
   @Input() public negocio: any;
-  public usuario: any;
-  public mostrarLike: any;
-  constructor(private util: UtilsCls) {
-    this.usuario = this.util.getUserData();
-    this.mostrarLike = this.util.existSession();
-  }
+  @Input() public usuario: any;
+  @Input() public mostrarLike: any;
+
+  constructor(
+    private servicioNegocio: ProveedorServicioService,
+    private notificacion: ToadNotificacionService
+  ) {}
 
   ngOnInit() {}
+
+  public darLike(negocio: any) {
+    this.servicioNegocio.darLike(negocio, this.usuario).subscribe(
+      (response) => {
+        if (response.code === 200) {
+          negocio.likes = response.data;
+          negocio.usuario_like = 1;
+          this.notificacion.exito(response.message);
+        } else {
+          negocio.likes = response.data;
+          negocio.usuario_like = 0;
+          this.notificacion.alerta(response.message);
+        }
+      },
+      (error) => {}
+    );
+  }
 }
