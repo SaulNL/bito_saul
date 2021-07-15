6011f863149c75e824eec4402dab832352267f0c
Shaila Palafox <shailapalafox@gmail.com>
2021-07-15 18:21:54 -0500

    ...

--- src/app/paginas/mis-favoritos/productos-favoritos/productos-favoritos.module.ts
+++ src/app/paginas/mis-favoritos/productos-favoritos/productos-favoritos.module.ts
@@ -6,16 +6,18 @@ import { IonicModule } from "@ionic/angular";
 import { ProductosFavoritosPageRoutingModule } from "./productos-favoritos-routing.module";
 import { ProductosFavoritosPage } from "./productos-favoritos.page";
 import { ModalProductoPageModule } from "../../productos/modal-producto/modal-producto.module";
+import {TabsPageModule} from '../../tabs/tabs.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    ProductosFavoritosPageRoutingModule,
-    ModalProductoPageModule,
-    SpinnerModule
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        ProductosFavoritosPageRoutingModule,
+        ModalProductoPageModule,
+        SpinnerModule,
+        TabsPageModule
+    ],
   exports: [ProductosFavoritosPage],
   declarations: [ProductosFavoritosPage],
 })
