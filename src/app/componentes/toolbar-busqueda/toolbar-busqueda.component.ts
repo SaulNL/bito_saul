64322cde0e618bdbed6f3c7094f07b00c4254a9b
Juan A <esjhuan1000@gmail.com>
2021-07-27 12:10:16 -0500

    busqueda

--- src/app/componentes/toolbar-busqueda/toolbar-busqueda.component.ts
+++ src/app/componentes/toolbar-busqueda/toolbar-busqueda.component.ts
@@ -27,7 +27,7 @@ export class ToolbarBusquedaComponent implements OnInit {
     private pedidosServicios: PedidosService,
     private _utils_cls: UtilsCls,
     private active: ActivatedRoute
-  ) { 
+  ) {
     this.totalNoVistos = 0;
     this.permisos = [];
     this.permisosList();
@@ -54,11 +54,7 @@ export class ToolbarBusquedaComponent implements OnInit {
   }
 
   buscar() {
-<<<<<<< HEAD
-
-=======
     //console.log(this.strBuscar)
->>>>>>> parent of bb19431 (log quitados)
     this.buscarEmit.emit(this.strBuscar);
   }
 
