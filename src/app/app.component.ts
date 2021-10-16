38d7a46df79e9bf55b18de12235c7f8ed36e0723
Omar Hernandez <40805237+omarirn@users.noreply.github.com>
2021-10-16 07:44:55 -0500

    deeplinks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -47,7 +47,7 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
-      //this.obtenerVersion();
+      this.obtenerVersion();
     });
     this.obtenerIP();
   }
