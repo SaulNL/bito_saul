3c3024fe9ad812f7696db81272bcfdd7b0766a2c
PaolaCoba <paola.cs@ce-patria.edu.mx>
2022-02-11 19:07:20 -0600

    Corrección de issue 176 y corrección de ortografía

--- src/app/Bitoo/components/modal-confirm-sign-up/modal-confirm-sign-up.component.ts
+++ src/app/Bitoo/components/modal-confirm-sign-up/modal-confirm-sign-up.component.ts
@@ -1,26 +1,27 @@
-import { ConfigGlobal } from './../../config/config-global';
-import { LoginService } from './../../../api/login.service';
-import { UserSignInInterface } from '../../models/user-sign-in-model';
-import { UsuarioService } from './../../../api/busqueda/login/usuario.service';
-import { ToadNotificacionService } from './../../../api/toad-notificacion.service';
-import { GeneralServicesService } from 'src/app/api/general-services.service';
-import { NgForm } from '@angular/forms';
-import { ShippingMethod } from './../../types/shipping-method-type';
-import { ModalController } from '@ionic/angular';
-import { ToSend } from './../../types/to-send-type';
-import { MessageTo } from './../../types/message-to-type';
-import { UserSignUpModel, UserSignUpInterface } from './../../models/user-sign-up-model';
-import { Component, Input, OnInit } from '@angular/core';
-import * as moment from 'moment';
+import { ConfigGlobal } from "./../../config/config-global";
+import { LoginService } from "./../../../api/login.service";
+import { UserSignInInterface } from "../../models/user-sign-in-model";
+import { UsuarioService } from "./../../../api/busqueda/login/usuario.service";
+import { ToadNotificacionService } from "./../../../api/toad-notificacion.service";
+import { GeneralServicesService } from "src/app/api/general-services.service";
+import { NgForm } from "@angular/forms";
+import { ShippingMethod } from "./../../types/shipping-method-type";
+import { ModalController } from "@ionic/angular";
+import { ToSend } from "./../../types/to-send-type";
+import { MessageTo } from "./../../types/message-to-type";
+import {
+  UserSignUpModel,
+  UserSignUpInterface,
+} from "./../../models/user-sign-up-model";
+import { Component, Input, OnInit } from "@angular/core";
+import * as moment from "moment";
 
 @Component({
-  selector: 'app-modal-confirm-sign-up',
-  templateUrl: './modal-confirm-sign-up.component.html',
-  styleUrls: ['./modal-confirm-sign-up.component.scss']
+  selector: "app-modal-confirm-sign-up",
+  templateUrl: "./modal-confirm-sign-up.component.html",
+  styleUrls: ["./modal-confirm-sign-up.component.scss"],
 })
-
 export class ModalConfirmSignUpComponent implements OnInit {
-
   @Input() public formSignUp: UserSignUpModel;
   @Input() public messageTo: MessageTo;
   @Input() public toSend: ToSend;
@@ -59,62 +60,68 @@ export class ModalConfirmSignUpComponent implements OnInit {
     this.closeModal();
   }
   public closeModal() {
-    this.deactivateTimer();
     this.modalCrl.dismiss();
+    this.deactivateTimer();
   }
 
   get isSendToMovil() {
-    return (this.shippingMethod === 1);
+    return this.shippingMethod === 1;
   }
 
   public getCodeSms() {
     this.retryCode = true;
-    this.usuarioService.getCode(this.formSignUp.ms_persona.telefono_celular, this.formSignUp.ms_persona.correo, this.shippingMethod).subscribe(
-      (response) => {
-        this.formSignUp.idCode = response.data;
-        this.initTimer();
-      }, () => {
-        this.failProcees();
-      }
-    );
+    this.usuarioService
+      .getCode(
+        this.formSignUp.ms_persona.telefono_celular,
+        this.formSignUp.ms_persona.correo,
+        this.shippingMethod
+      )
+      .subscribe(
+        (response) => {
+          this.formSignUp.idCode = response.data;
+          this.initTimer();
+        },
+        () => {
+          this.failProcees();
+        }
+      );
   }
   public initTimer() {
     let min: any;
     let sec: any;
     let duration: moment.Duration = moment.duration({
       minutes: 0,
-      seconds: this.timeTimer + 2
+      seconds: this.timeTimer + 2,
     });
     let time = new Date(0, 0, 0, 2, 10, 30);
     const intervals: number = 1;
-    this.timer = setInterval(
-      function () {
-        time = new Date(time.getTime() + intervals * 1000);
-        duration = moment.duration(duration.asSeconds() - intervals, 'seconds');
-        min = duration.minutes();
-        sec = duration.seconds();
-        sec -= 1;
-        if (min < 0) {
-          return clearInterval(this.timer);
-        }
-        if (min < 10 && min.length !== 2) {
-          min = '0' + min;
-        }
-        if (sec < 0 && min !== 0) {
-          min -= 1;
-          sec = 59;
-        } else if (sec < 10 && sec.length !== 2) {
-          sec = '0' + sec;
-        }
-        let label: HTMLElement = document.getElementById('timer');
+    this.timer = setInterval(function () {
+      time = new Date(time.getTime() + intervals * 1000);
+      duration = moment.duration(duration.asSeconds() - intervals, "seconds");
+      min = duration.minutes();
+      sec = duration.seconds();
+      sec -= 1;
+      if (min < 0) {
+        return clearInterval(this.timer);
+      }
+      if (min < 10 && min.length !== 2) {
+        min = "0" + min;
+      }
+      if (sec < 0 && min !== 0) {
+        min -= 1;
+        sec = 59;
+      } else if (sec < 10 && sec.length !== 2) {
+        sec = "0" + sec;
+      }
+      let label: HTMLElement = document.getElementById("timer");
 
-        label.innerText = min + ':' + sec;
-        if (min == 0 && sec == 0) {
-          clearInterval(this.timer);
-          clearInterval(this.disableTimer);
-          document.getElementById("timer").innerHTML = '';
-        }
-      }, 1000);
+      label.innerText = min + ":" + sec;
+      if (min == 0 && sec == 0) {
+        clearInterval(this.timer);
+        clearInterval(this.disableTimer);
+        document.getElementById("timer").innerHTML = "";
+      }
+    }, 1000);
     this.retryCode = true;
     this.disableTimer = setTimeout(() => {
       this.deactivateTimer();
@@ -124,7 +131,7 @@ export class ModalConfirmSignUpComponent implements OnInit {
   public deactivateTimer() {
     clearInterval(this.timer);
     clearInterval(this.disableTimer);
-    document.getElementById("timer").innerHTML = '';
+    document.getElementById("timer").innerHTML = "";
     this.retryCode = false;
   }
 
@@ -142,20 +149,24 @@ export class ModalConfirmSignUpComponent implements OnInit {
       (response) => {
         if (response.code === 200) {
           const signIn: UserSignInInterface = {
-            usuario: (this.shippingMethod === 1) ? this.formSignUp.ms_persona.telefono_celular : this.formSignUp.ms_persona.correo,
+            usuario:
+              this.shippingMethod === 1
+                ? this.formSignUp.ms_persona.telefono_celular
+                : this.formSignUp.ms_persona.correo,
             password: this.formSignUp.password,
-            type: ''
+            type: "",
           };
           this.loginService.validateLogin(signIn).subscribe(
             (r) => {
               this.toadNotificationService.alerta(r.message);
               if (r.code === 200) {
                 ConfigGlobal.setUser(r);
-                this.toadNotificationService.exito('Bienvenido a Bitoo');
-                this.modalCrl.dismiss({ 'success': true });
+                this.toadNotificationService.exito("Bienvenido a Bitoo");
+                this.modalCrl.dismiss({ success: true });
                 this.loaderCreateAccount = false;
               }
-            }, (error) => {
+            },
+            (error) => {
               this.loaderCreateAccount = false;
               this.toadNotificationService.error(error);
             }
@@ -164,7 +175,8 @@ export class ModalConfirmSignUpComponent implements OnInit {
           this.loaderCreateAccount = false;
           this.toadNotificationService.alerta(response.message);
         }
-      }, (error) => {
+      },
+      (error) => {
         this.loaderCreateAccount = false;
         this.toadNotificationService.error(error);
       }
@@ -175,15 +187,15 @@ export class ModalConfirmSignUpComponent implements OnInit {
     this.generalService.obtenerTiempoTemporizador().subscribe(
       (response) => {
         this.timeTimer = response.data;
-      }, () => {
+      },
+      () => {
         this.failProcees();
       }
     );
   }
 
   public failProcees() {
-    this.toadNotificationService.error('Error en el servidor');
+    this.toadNotificationService.error("Error en el servidor");
     this.closeModal();
   }
-
 }
