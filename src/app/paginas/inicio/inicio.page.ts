8971fb00af17d97da4da324410c5269bc616eadc
Beresnz <beressnz19@gmail.com>
2022-02-23 22:43:30 -0600

    201 Abierto/Cerrado

--- src/app/paginas/inicio/inicio.page.ts
+++ src/app/paginas/inicio/inicio.page.ts
@@ -225,6 +225,9 @@ export class InicioPage implements OnInit {
 
   cargarCategorias() {
 
+    this.Filtros.tipoBusqueda=1;
+    this.Filtros.idEstado=29;
+
     this.principalSercicio
       .obtenerNegocioPorCategoria(this.Filtros, this.siguientePagina)
       .then((respuesta) => {
