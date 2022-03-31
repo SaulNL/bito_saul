1d2850960ea00e7236b1c93f2af384b730d5d13e
Beresnz <beressnz19@gmail.com>
2022-03-31 11:40:39 -0600

    Correccion de fecha de filtro en ventas

--- src/app/paginas/pedidos/pedidos-negocio/pedidos-negocio.page.ts
+++ src/app/paginas/pedidos/pedidos-negocio/pedidos-negocio.page.ts
@@ -10,6 +10,10 @@ import {Router, ActivatedRoute} from "@angular/router";
 import {LoadingController} from "@ionic/angular";
 import {Location} from "@angular/common";
 
+import { IPedidosNegocios } from "src/app/interfaces/pedidos/IPedidosNegocio";
+  
+  import { IFiltroBusqueda } from "src/app/interfaces/pedidos/IFiltroBusqueda";
+  
 @Component({
     selector: "app-pedidos-negocio",
     templateUrl: "./pedidos-negocio.page.html",
@@ -27,6 +31,9 @@ export class PedidosNegocioPage implements OnInit {
     public loaderBus = false;
     public loaderEst = false;
     public cargando = 'Cargando';
+    public  inicioFecha:String ;
+    public  finalFecha:String ;
+
 
     constructor(
         private location: Location,
@@ -36,10 +43,12 @@ export class PedidosNegocioPage implements OnInit {
         private router: Router,
         private active: ActivatedRoute,
         public loadingController: LoadingController,
-        private platform: Platform
+        private platform: Platform,
     ) {
         this.listaNegocioPedididos = [];
         this.lstFiltroEstatus = [1, 2, 3, 4, 5, 6];
+        this.inicioFecha="";
+        this.finalFecha="";
     }
 
     ngOnInit() {
@@ -62,6 +71,130 @@ export class PedidosNegocioPage implements OnInit {
         });
     }
 
+    format(fecha: String) {
+        var cadena = fecha.replace("/", "-");
+        var dia = cadena.substring(0, cadena.indexOf("-"));
+        var copia = cadena.substring(cadena.indexOf("-") + 1);
+        var mes = copia.substring(0, copia.indexOf("/"));
+        var year = cadena.substring(cadena.lastIndexOf("/") + 1);
+        cadena = year + "-" + mes + "-" + dia;
+        var salida = Date.parse(cadena);
+        var fecha2 = new Date(salida);
+        return fecha2;
+      }
+      dayFecha() {
+        var actual= new Date();
+        var mon= ""+(actual.getMonth() + 1);
+        if(mon.length==1){
+          mon="0"+mon;
+        }
+        var day= ""+actual.getDate();
+        if(day.length==1){
+          day="0"+day;
+        }
+        return actual.getFullYear() + "-" +mon + "-" + day;
+    
+      }
+      weekFecha() {
+        var actual= new Date();
+        actual.setDate(actual.getDate() - 7);
+        var mon= ""+(actual.getMonth() + 1);
+        if(mon.length==1){
+          mon="0"+mon;
+        }
+        var day= ""+actual.getDate();
+        if(day.length==1){
+          day="0"+day;
+        }
+        return actual.getFullYear() + "-" +mon + "-" + day;
+    
+      }
+      
+      format2(fecha: String) {
+        var cadena = fecha.substring(0, fecha.indexOf("T"));
+        return cadena;
+      }
+    
+      moothFecha() {
+        var actual= new Date();
+        actual.setDate(actual.getDate() - 30);
+        var mon= ""+(actual.getMonth() + 1);
+        if(mon.length==1){
+          mon="0"+mon;
+        }
+        var day= ""+actual.getDate();
+        if(day.length==1){
+          day="0"+day;
+        }
+        return actual.getFullYear() + "-" +mon + "-" + day;
+    
+      }
+    
+      clickEvaluar(opcion:number) {
+        const id = this.utilsCls.getIdProveedor();
+        this.loaderBus = true;
+        this.loaderSearch = true;
+        let pedidosNegocios: IPedidosNegocios = {
+          id_proveedor: id,
+          estatus: this.lstFiltroEstatus
+        };
+        if(opcion==1){
+          pedidosNegocios.fecha_inicio =this.dayFecha();
+          pedidosNegocios.fecha_final = this.dayFecha();
+        }
+        if(opcion==2){
+          pedidosNegocios.fecha_inicio =this.weekFecha();
+          pedidosNegocios.fecha_final = this.dayFecha();
+        }
+        if(opcion==3){
+          pedidosNegocios.fecha_inicio =this.moothFecha();
+          pedidosNegocios.fecha_final = this.dayFecha();
+        }
+        this.pedidosServicios
+        .pedidosNegocios2(pedidosNegocios)
+        .subscribe(
+          (res) => {
+            //this.loader = false;
+            this.listaNegocioPedididos = res.data;
+            this.loaderBus = false;
+            this.loaderSearch = false;
+          },
+          (error) => {
+            this.loaderBus = false;
+            this.loaderSearch = false;
+            //this.loader = false;
+          }
+        );
+      }
+      fechasRengo(){
+        const id = this.utilsCls.getIdProveedor();
+        this.loaderBus = true;
+        this.loaderSearch = true;
+        let pedidosNegocios: IPedidosNegocios = {
+          id_proveedor: id,
+          estatus: this.lstFiltroEstatus
+        };
+    
+        pedidosNegocios.fecha_inicio =this.format2(this.inicioFecha);
+        pedidosNegocios.fecha_final = this.format2(this.finalFecha);
+        this.pedidosServicios
+        .pedidosNegocios2(pedidosNegocios)
+        .subscribe(
+          (res) => {
+            //this.loader = false;
+            this.listaNegocioPedididos = res.data;
+            this.loaderBus = false;
+            this.loaderSearch = false;
+          },
+          (error) => {
+            this.loaderBus = false;
+            this.loaderSearch = false;
+            //this.loader = false;
+          }
+        );
+      }
+    
+
     buscar() {
         const id = this.utilsCls.getIdProveedor();
         this.pedidosServicios
@@ -78,6 +211,17 @@ export class PedidosNegocioPage implements OnInit {
                 }
             );
     }
+
+    filtroParaBusquedas(filtroBusqueda: IFiltroBusqueda | null,
+        esParaBuscaTodos: boolean = false) {
+    
+        if (filtroBusqueda) {
+          /*  this.fechaFinal = filtroBusqueda.fechaFinal;
+            this.fechaInicio = filtroBusqueda.fechaInicio;*/
+        }
+        this.buscar()
+    
+    }    
 
     buscarEstatus() {
         const id = this.utilsCls.getIdProveedor();
