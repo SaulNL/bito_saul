77f7620d8968cc6277aab3df233244fb75b6f731
Omar Hernandez <omar8641975321@gmail.com>
2022-05-05 17:18:56 -0500

    Cambio de tab para IOS

--- src/app/paginas/tabs/tabs.page.ts
+++ src/app/paginas/tabs/tabs.page.ts
@@ -3,6 +3,7 @@ import { UtilsCls } from "../../utils/UtilsCls";
 import { SideBarService } from "../../api/busqueda/side-bar-service";
 import { Auth0Service } from "../../api/busqueda/auth0.service";
 import { Router } from "@angular/router";
+import {Platform} from "@ionic/angular";
 
 @Component({
   selector: "app-tabs",
@@ -14,15 +15,18 @@ export class TabsPage implements OnInit {
   existeSesion: boolean;
   usuario: any;
   activedPage: string;
+  public isIos: boolean;
 
   constructor(
-    private util: UtilsCls,
-    private sideBarService: SideBarService,
-    private router: Router,
-    private auth0: Auth0Service
+      private util: UtilsCls,
+      private sideBarService: SideBarService,
+      private router: Router,
+      private auth0: Auth0Service,
+      private platform: Platform,
   ) {
     this.existeSesion = util.existe_sesion();
     this.activedPage = "";
+    this.isIos = this.platform.is('ios');
   }
 
   ngOnInit(): void {
@@ -33,17 +37,22 @@ export class TabsPage implements OnInit {
       this.usuario = this.auth0.getUserData();
     });
     this.usuario = this.util.getData();
-    let pagina = localStorage.getItem('activedPage');
-     if(pagina==='promociones'){
+    const pagina = localStorage.getItem('activedPage');
+
+    if (pagina === null && this.isIos){
+      this.activedPage = 'inicio';
+    }
+
+    if(pagina==='promociones'){
       this.activedPage = localStorage.getItem("activedPage");
-     }
-     if(pagina==='productos'){
+    }
+    if(pagina==='productos'){
       this.activedPage = localStorage.getItem("activedPage");
-     }
-     if(pagina==='perfil'){
+    }
+    if(pagina==='perfil'){
       this.activedPage = localStorage.getItem("activedPage");
-     }
-     localStorage.removeItem('activedPage');
+    }
+    localStorage.removeItem('activedPage');
   }
 
   inicio() {
