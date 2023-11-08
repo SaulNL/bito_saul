14cd82209e4980e05e345deac83cb3a80adb9ae3
fsamayoa <packosamayoa@gmail.com>
2023-11-08 15:41:50 -0600

    Se importo el componente de modal info solicitud

--- src/app/paginas/notificacion/notificacion.module.ts
+++ src/app/paginas/notificacion/notificacion.module.ts
@@ -8,6 +8,7 @@ import { NotificacionPageRoutingModule } from './notificacion-routing.module';
 
 import { NotificacionPage } from './notificacion.page';
 import { NotificacionChatComponent } from '../../components/notificacion-chat/notificacion-chat.component';
+import { ModalInfoSolicitudComponent } from '../../componentes/modal-info-solicitud/modal-info-solicitud.component';
 //import {SpinnerComponent} from "../../componentes/spinner/spinner.component";
 
 @NgModule({
@@ -20,6 +21,7 @@ import { NotificacionChatComponent } from '../../components/notificacion-chat/no
   declarations: [
     NotificacionPage, 
     NotificacionChatComponent,
+      ModalInfoSolicitudComponent
   ]
 })
 export class NotificacionPageModule {}
